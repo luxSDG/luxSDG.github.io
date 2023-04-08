3728e7687f962cb040c232617fb58469686cfc64
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2023-04-07 22:10:31 -0700

    role list created and links set

--- Opsec C/opsecCRoles.js
+++ Opsec C/opsecCRoles.js
@@ -4,17 +4,21 @@ for (let i = 0; i < data.length; i++)
 {
 	var category = data[i];
 
-	//Categories for each opsec C role, e.g. faction and/or subclass
-	var categoryName = Object.keys(category);
-	content.innerHTML += categoryName[0] + "<br>";
-
 	//Specific roles per category
 	var roleList = Object.values(category)[0];
+	var categoryColor = roleList[0]["Color"];
+
+	//Categories for each opsec C role, e.g. faction and/or subclass
+	var categoryName = Object.keys(category);
+	content.innerHTML += "<span style=\"color:" + categoryColor + "\">" + categoryName[0] + "</span><br>";
+
 	for (let j = 0; j < roleList.length; j++)
 	{
 		var role = roleList[j];
 		var roleName = role["Name"];
-		content.innerHTML += roleName + "<br>";
+		var roleSummary = role["Summary"];
+		content.innerHTML += "&emsp;<span style=\"color:" + categoryColor + "\"><a href=\"opsecCRoleSpecific.html\" style=\"color:"
+		 + categoryColor + "\">" + roleName + "</a> - " + roleSummary + "</span><br>";
 	}
 	content.innerHTML += "<br>";
 }
