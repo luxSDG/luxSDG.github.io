3f14087c1647dea8ade16991a71feee3e818ea1c
Michael Yuen <34826088+luxebo@users.noreply.github.com>
2024-08-06 19:24:10 -0700

    variable assignment

--- Pawns of Despair/podUtility.js
+++ Pawns of Despair/podUtility.js
@@ -1,1 +1,53 @@
+//PoD Utility
+
+//Attack/Heal
+const dmg = document.getElementById("dmg");
+const maxHPdmg = document.getElementById("maxHPdmg");
+const curHPdmg = document.getElementById("curHPdmg");
+const empower = document.getElementById("empower");
+const exhaust = document.getElementById("exhaust");
+const heal = document.getElementById("heal");
+const shieldA = document.getElementById("shieldA");
+const apCost = document.getElementById("apCost");
+
+//Status
+const slowA = document.getElementById("slowA");
+const disruptA = document.getElementById("disruptA");
+const vulnerableA = document.getElementById("vulnerableA");
+const exhaustA = document.getElementById("exhaustA");
+const weakenA = document.getElementById("weakenA");
+const suppressA = document.getElementById("suppressA");
+const push = document.getElementById("push");
+const pull = document.getElementById("pull");
+
+//Buff
+const hasteA = document.getElementById("hasteA");
+const clarityA = document.getElementById("clarityA");
+const braceA = document.getElementById("braceA");
+const empowerA = document.getElementById("empowerA");
+const strengthenA = document.getElementById("strengthenA");
+const tenacityA = document.getElementById("tenacityA");
+
+//Stats
+const hp = document.getElementById("hp");
+const maxHP = document.getElementById("maxHP");
+const shieldD = document.getElementById("shieldD");
+const ap = document.getElementById("ap");
+const range = document.getElementById("range");
+
+//buffs
+const hasteD = document.getElementById("hasteD");
+const clarityD = document.getElementById("clarityD");
+const braceD = document.getElementById("braceD");
+const empowerD = document.getElementById("empowerD");
+const strengthenD = document.getElementById("strengthenD");
+const tenacityD = document.getElementById("tenacityD");
+
+//stats
+const slowD = document.getElementById("slowD");
+const disruptD = document.getElementById("disruptD");
+const vulnerableD = document.getElementById("vulnerableD");
+const exhaustD = document.getElementById("exhaustD");
+const weakenD = document.getElementById("weakenD");
+const suppressD = document.getElementById("suppressD");
 
